8acf986deda9e9183bcfcef73d307613426f3de4
Dev-Joon <closeplz0526@gmail.com>
2021-12-13 08:08:30 +0900

    gui_basic update

---
+++ python/mystudy/gui_basic/3_label.py
@@ -0,0 +1,27 @@
+from tkinter import *
+import os
+
+root = Tk()
+root.title("Nado GUI")
+root.geometry("640x480") # 가로 * 세로
+
+label1 = Label(root, text="안녕하세요")
+label1.pack()
+
+file_path = os.path.dirname(__file__)
+photo = PhotoImage(file=os.path.join(file_path, "img.png"))
+label2 = Label(root, image=photo)
+label2.pack()
+
+def change():
+    label1.config(text="또 만나요")
+
+    global photo2
+    photo2 = PhotoImage(file=os.path.join(file_path, "img2.png"))
+    label2.config(image=photo2)
+
+btn = Button(root, text="클릭", command=change)
+btn.pack()
+
+root.mainloop()
+
---
+++ python/mystudy/gui_basic/4_text_entry.py
@@ -0,0 +1,11 @@
+from tkinter import *
+import os
+
+root = Tk()
+root.title("Nado GUI")
+root.geometry("640x480") # 가로 * 세로
+
+
+
+root.mainloop()
+
