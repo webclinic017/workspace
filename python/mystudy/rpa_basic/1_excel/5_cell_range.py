117015830aa8a824f88ae83c9ce3129bbf15b164
Dev-Joon <closeplz0526@gmail.com>
2021-12-27 22:26:37 +0900

    rpa_basic > 1_excel > 5_cell_range

--- python/mystudy/rpa_basic/1_excel/5_cell_range.py
+++ python/mystudy/rpa_basic/1_excel/5_cell_range.py
@@ -29,17 +29,41 @@ row_title = ws[1] # 1번째 row만 가지고 오기
 #         print(cell.value, end = " ")
 #     print()
 
-from openpyxl.utils.cell import coordinate_from_string
+# from openpyxl.utils.cell import coordinate_from_string
 
-row_range = ws[2:ws.max_row]    # 2번째 줄부터 마지막 줄까지
-for rows in row_range:
-    for cell in rows:
-        # print(cell.value, end = " ")
-        #print(cell.coordinate, end = " ")
-        xy = coordinate_from_string(cell.coordinate)
-        #print(xy, end=" ")
-        print(xy[0], end="") # A
-        print(xy[1], end=" ") # B
-    print()
+# row_range = ws[2:ws.max_row]    # 2번째 줄부터 마지막 줄까지
+# for rows in row_range:
+#     for cell in rows:
+#         # print(cell.value, end = " ")
+#         #print(cell.coordinate, end = " ")
+#         xy = coordinate_from_string(cell.coordinate)
+#         #print(xy, end=" ")
+#         print(xy[0], end="") # A
+#         print(xy[1], end=" ") # B
+#     print()
+
+# 전체 rows
+# print(tuple(ws.rows))
+# for row in tuple(ws.rows):
+#     print(row[1].value)
+
+# 전체 columns
+#print(tuple(ws.columns))
+# for column in tuple(ws.columns):
+#     print(column[0].value)
+
+# for row in ws.iter_rows(): # 전체 row
+#     print(row[1].value)
+
+# for column in ws.iter_cols(): # 전체 column
+#     print(column[2].value)
+
+# 2번째 줄부터 11번쨰 줄까지, 2번째 열부터 3번째 열까지
+# for row in ws.iter_rows(min_row=2, max_row=11, min_col=2, max_col=3):  # 전체 row
+#     # print(row[0].value, row[1].value)
+#     print(row)
+    
+for col in ws.iter_cols(min_row=1, max_row=5, min_col=1, max_col=3):
+    print(col)
 
 wb.save("sample.xlsx")
